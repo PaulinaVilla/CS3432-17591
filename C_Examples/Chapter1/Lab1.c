126c66ec14c110fd127b1f7183f5a7ad7f86bb6e
PaulinaVilla <pevilla@miners.utep.edu>
2021-09-20 10:09:00 -0600

    Lab1.c

--- C_Examples/Chapter1/Lab1.c
+++ C_Examples/Chapter1/Lab1.c
@@ -11,6 +11,11 @@
    Zero terminators are not printable (therefore false) */
 
 bool delim_character(char c){
+  if(c == ' ' || c == '\t'){
+    return true;
+  }else{
+    return false;
+  }
 
 
 
@@ -25,7 +30,11 @@ bool delim_character(char c){
    Zero terminators are not printable (therefore false) */
 
 bool non_delim_character(char c){
-
+  if(c == ' ' || c == '\t'){
+    return false;
+  }else{
+    return true;
+  }
 
 
 }
@@ -37,7 +46,11 @@ bool non_delim_character(char c){
    space-separated word*/
 
 char *word_start(char* str){
-
+  for(i = 0;*(str + i)!='\0';i++){
+    if(non_delim_character(*(str+1)){
+	return str + i;
+      }
+      }
 
 
 }
@@ -49,7 +62,12 @@ char *word_start(char* str){
    terminated string*/
 
 char *end_word(char* str){
-
+  char *first = word_start(str)
+    for(i = 0;*(first + 1) != '\0';i++){
+      if(delim_character(*(first + i + 1)){
+	  return first + 1;
+	}
+	}
 
 
 }
@@ -63,6 +81,27 @@ int count_tokens(char* str){
 
 
 }
+char *copy_str(char *inStr, short len){
+
+
+
+}
+
+
+
+char** tokenize(char* str){
+
+}
+
+
+
+
+
+void print_all_tokens(char** tokens){
+
+}
+
+
 
 /* Returns a freshly allocated zero-terminated vector of freshly allocated
 
